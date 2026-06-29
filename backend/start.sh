264475c954929cb4e00d112ad873c5808fdc3f96
futurekelly <futurekelly360@gmail.com>
2026-06-30 02:50:03 +0300

    add start.sh for production deployment

---
+++ backend/start.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+echo "Running migrations..."
+python manage.py migrate --noinput
+
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
+
+echo "Starting Gunicorn server..."
+gunicorn church_nexus.wsgi:application --bind 0.0.0.0:10000 --workers 2
